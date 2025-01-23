6021af37d3520d9e8225767f5a39c6adf6435e90
Toavina Sylvianno <toavinasylvianno14@gmail.com>
2025-01-23 22:19:00 +0300

    AJout d'un scroll vertical pour les devices avec des ecrans tres petits

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Image, StyleSheet, Platform } from 'react-native';
 import  LivraisonScreen  from '@/components/ui/livraison/LivraisonScreen';
-import { View } from 'react-native';
+import { ScrollView , View } from 'react-native';
 
 export default function HomeScreen() {
   const user = {
 
   }
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <LivraisonScreen userName='Marvis Kparobo' userAddress='Km 5 refinery road opposite re public road, efferum, delta state' userTelephone='+234 9011039271' totalPrice={24000}/>
-    </View>
+    <ScrollView>
+      <View  style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <LivraisonScreen userName='Marvis Kparobo' userAddress='Km 5 refinery road opposite re public road, efferum, delta state' userTelephone='+234 9011039271' totalPrice={24000}/>
+      </View>
+    </ScrollView>
   );
 }
 
